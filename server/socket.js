e540be089b4d122bfb11aafcd447409b3d21d74c
dejakob <jakobvti@gmail.com>
2015-03-10 21:30:15 +0100

    introducing domains

--- server/socket.js
+++ server/socket.js
@@ -2,5 +2,11 @@ var WebSocketServer = require('ws').Server
     , wss = new WebSocketServer({port: 8889});
 console.log('loading socket...');
 wss.on('connection', function(ws) {
-    require(GLOBAL.rootpath + '/server/spacelogic/spacesocket.js')(ws);
+    var d = require('domain').create();
+    d.on('error', function(err) {
+
+    });
+    d.run(function() {
+        require(GLOBAL.rootpath + '/server/spacelogic/spacesocket.js')(ws);
+    });
 });
